1efed2a8bfdc3e882e5d41a70266f3cdc30b6313
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "signalr_service_custom_certificates" {
-  description = "All signalr_service_custom_certificate resources"
-  value       = azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates
-}
 output "signalr_service_custom_certificates_certificate_version" {
-  description = "List of certificate_version values across all signalr_service_custom_certificates"
-  value       = [for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : v.certificate_version]
+  description = "Map of certificate_version values across all signalr_service_custom_certificates, keyed the same as var.signalr_service_custom_certificates"
+  value       = { for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : k => v.certificate_version }
 }
 output "signalr_service_custom_certificates_custom_certificate_id" {
-  description = "List of custom_certificate_id values across all signalr_service_custom_certificates"
-  value       = [for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : v.custom_certificate_id]
+  description = "Map of custom_certificate_id values across all signalr_service_custom_certificates, keyed the same as var.signalr_service_custom_certificates"
+  value       = { for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : k => v.custom_certificate_id }
 }
 output "signalr_service_custom_certificates_name" {
-  description = "List of name values across all signalr_service_custom_certificates"
-  value       = [for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : v.name]
+  description = "Map of name values across all signalr_service_custom_certificates, keyed the same as var.signalr_service_custom_certificates"
+  value       = { for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : k => v.name }
 }
 output "signalr_service_custom_certificates_signalr_service_id" {
-  description = "List of signalr_service_id values across all signalr_service_custom_certificates"
-  value       = [for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : v.signalr_service_id]
+  description = "Map of signalr_service_id values across all signalr_service_custom_certificates, keyed the same as var.signalr_service_custom_certificates"
+  value       = { for k, v in azurerm_signalr_service_custom_certificate.signalr_service_custom_certificates : k => v.signalr_service_id }
 }
 
